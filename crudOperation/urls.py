4bebfc2b229567e07a51e56cdd72acb3318b17e8
Ravindra <kauthaleravindra@gmail.com>
2021-10-04 20:20:49 +0530

    Updated urls file

--- crudOperation/urls.py
+++ crudOperation/urls.py
@@ -13,18 +13,11 @@ Including another URLconf
     1. Import the include() function: from django.urls import include, path
     2. Add a URL to urlpatterns:  path('blog/', include('blog.urls'))
 """
-from django.contrib import admin
-from django.urls import path
 
 from django.contrib import admin
-from django.urls import path
+from django.urls import path, include
 from employee import views
 urlpatterns = [
     path('admin/', admin.site.urls),
-    path('', views.emp),
-    path('emp', views.emp),
-    path('show', views.show),
-    path('edit/<int:id>', views.edit),
-    path('update/<int:id>', views.update),
-    path('delete/<int:id>', views.destroy),
+    path('', include('employee.urls')),
 ]
